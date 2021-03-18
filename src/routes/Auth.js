5d8319eea36f61bb46a37e7abc99b4d0ec945a29
sumin03 <breath.ing0210@gmail.com>
2021-03-19 02:50:10 +0900

    2.1 Login base

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 //If I write like this, then It should be automatically imported
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Goggle</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
+
 export default Auth;
