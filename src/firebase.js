a1e7919708ef141b4d13b6cdb0aad99508449b9e
sumin03 <breath.ing0210@gmail.com>
2021-03-18 22:06:24 +0900

    1.2 firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBP1E2DmTowEDUr4P0UzqdL7KNrmaTawxs",
-  authDomain: "nwitter-f4e34.firebaseapp.com",
-  projectId: "nwitter-f4e34",
-  storageBucket: "nwitter-f4e34.appspot.com",
-  messagingSenderId: "880941464505",
-  appId: "1:880941464505:web:7d261cb9df77af5ed09468",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
+// Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
